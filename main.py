46aad803b99d2dc07f968f58ed52e0f2b425f62a
Azmi Azhar <azmiazharm@gmail.com>
2021-06-08 10:44:10 +0700

    refactor main flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, request
+# Import lib
+from flask import Flask, request, jsonify, make_response
 from keras.preprocessing import image
 import numpy as np
 import tensorflow as tf
@@ -7,28 +7,35 @@ from PIL import Image
 import io
 import time
 
-model = tf.keras.models.load_model('./model/malaria-90.h5')
+# Import ML model
+malaria_model = tf.keras.models.load_model('./model/malaria-90.h5')
 
+# Define flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Helloo</h1>'
+    return '<h1>EzCell Backend Service</h1>'
 
 
-@app.route('/process', methods=['POST'])
-def infer_image():
+@app.route('/process-malaria', methods=['POST'])
+def process_malaria():
+    start = time.time()
+
     if 'file' not in request.files:
-        return "Image missing"
+        return make_response(jsonify(success=False, message='File not found'), 400)
 
     file = request.files.get('file')
     if not file:
-        return "File not found"
+        return make_response(jsonify(success=False, message='File not found'), 400)
 
     img = resize_img(file.read())
 
-    result = predict(img)
-    return result
+    result = predict_malaria(img)
+
+    time_taken = '{} s'.format(round(time.time() - start, 2))
+    print(time_taken)
+    return jsonify(success=True, result=result, process_time=time_taken)
 
 def resize_img(img):
     img = Image.open(io.BytesIO(img))
@@ -37,30 +44,13 @@ def resize_img(img):
     img = np.expand_dims(img, axis=0)
     return np.vstack([img])
 
-def predict(img):
-    a = getTime()   
-    print(a)
-    classes = model.predict(img, batch_size=10)
+def predict_malaria(img):
+    classes = malaria_model.predict(img, batch_size=10)
 
-    print(getTime())
-    print(classes)
-    print(classes[0])
-
-    
     if classes[0]>0:
-        return {
-            "result" : "uninfected",
-            "process" : str(round(getTime() - a, 2)) + " s"
-            }
+        return "uninfected"
     else:
-        return {
-            "result" : "infected",
-            "process" : str(round(getTime() - a, 2)) + " s"
-            }
-
-def getTime():
-    return time.time()
-
+        return "infected"
 
 if(__name__ == "__main__"):
     app.run(debug=True)
